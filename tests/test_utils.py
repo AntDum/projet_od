fe22c26296b751c251c8a73ac7e4ff8d6583b015
AntDum <antoine.dumoulin@student.uclouvain.be>
2022-03-21 13:32:55 +0100

    little testing

---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+import unittest
+import projet_od.utils
+
+class Test_Utils(unittest.TestCase):
+    def test_norm(self):
+        pass
+
+    def test_lerp(self):
+        pass
+
+    def test_map(self):
+        pass
+
+    def test_clamp(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
